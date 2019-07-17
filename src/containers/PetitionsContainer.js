135d69fc1cd5197c867902f5e5e83db6489a8f52
NLT <NamibiaLT@users.noreply.github.com>
2019-07-17 00:10:00 -0700

    add loading feature

--- src/containers/PetitionsContainer.js
+++ src/containers/PetitionsContainer.js
@@ -1,40 +1,58 @@
-import React, { Component } from 'react'
-import axios from 'axios'
+import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class PetitionsContainer extends Component {
-  getPetitionsList() {
+
+  // state = {
+  //   petitions: [],
+  //   page: 1,
+  //   pageSize: 10,
+  //   sortBy: 'created_at'
+  // }
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      petitions: [],
+      page: 1,
+      pageSize: 10,
+      sortBy: 'created_at'
+    };
+
+    this.onLoadPetitions = this.onLoadPetitions.bind(this);
   }
 
-  state = {
 
-    petitions: [],
-    page: 1,
-    pageSize: 15,
-    sortBy: 'created_at'
-
+  onLoadPetitions(e) {
+      e.preventDefault();
+      this.setState(prevState => {
+        return {pageSize: this.state.pageSize.count + 5 }
+      })
   }
 
   componentDidMount() {
     axios.get(`http://localhost:3001/petitions?size=${this.state.pageSize}&sortBy=${this.state.sortBy}`)
       .then(res => {
-        this.setState({petitions: res.data});
+        this.setState({petitions: res.data.items});
       })
   }
 
   render() {
-    const {petitions} = this.state
+    const {petitions, pageSize} = this.state
     let petitionsList = (
     <ul>
       {petitions && petitions.map((petition) => {
-        return <li>{petition.id}</li>
+        return <li key={petition.display_title}>{petition.display_title}</li>
       })}
     </ul>
     )
 
+
     return (
       <div>
-        <h1>These are the trending petitions</h1>
-
+        <h1>These are trending petitions</h1>
+        {petitions.length > 0 ? petitionsList : "loading"}
+        <a href="#" onClick={this.onLoadPetitions}>Load more petitions...</a>
       </div>
     );
   }
